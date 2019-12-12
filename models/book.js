079dfe09f18b766612df4ce2aec928da2df7fdb0
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2019-12-12 21:30:24 +0100

    Update book model

--- models/book.js
+++ models/book.js
@@ -1,41 +1,46 @@
 const mongoose = require('mongoose');
 const Joi = require("joi");
 
+const currentYear = new Date().getFullYear();
 
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, "Title should be added"],
-        min: 2,
-        max: 100
+        minlength: 2,
+        maxlength: 100
     },
     author: {
-        type: [{
-          type: String,
-          required: [true, "Author should be added"],
-          min: 4,
-          max: 50
-      }]
+        type: String,
+        required: [true, "Author should be added"],
+        minlength: 4,
+        maxlength: 200
     },
-
+    description: {
+        type: String,
+        required: [true, "Description should be added"],
+        minlength: 10,
+        maxlength: 1000
+    },
     category: {
         type: String,
         required: [true, "Category should be added"],
         enum: [
-            "Thriller",
-            "Crime fiction",
-            "Romance",
-            "For kids",
-            "Biography",
-            "Fantasy",
-            "Handbooks",
-            "Poetry",
-            "Travel",
-            "Non-fiction"
+          "Thriller",
+          "Crime fiction",
+          "Romance",
+          "For kids",
+          "Biography",
+          "Fantasy",
+          "Handbooks",
+          "Poetry",
+          "Travel",
+          "Non-fiction"
         ]
     },
     publishingYear: {
         type: Number,
+        max: currentYear,
         required: [true, "Publishing year should be added"]
     },
     reviewsList: [{ 
@@ -43,22 +48,18 @@ const bookSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
         },
-        body: {
+        comment: {
             type: String,
+            minlength: 5,
             maxlength: 200
         }, 
         date: {
-            type: Date,
-            default: Date.now
+            type: String
         },
         stars: {
             type: Number,
             min: 0,
             max: 5,
-            validate: {
-                validator: Number.isInteger,
-                message: "Stars amount has to be integer"
-            }
         }
     }]
 });
@@ -67,16 +68,19 @@ const Book = mongoose.model('Book', bookSchema);
 
 function validateBook(book) {
     const schema = {
-        title: Joi.string
+        title: Joi.string()
             .required()
             .min(2)
             .max(100),
-        author: Joi.array().items(Joi.string()
+        author: Joi.string()
             .required()
             .min(4)
-            .max(50)
-            ),
-        category: Joi.string
+            .max(200),
+        description: Joi.string()
+            .required()
+            .min(10)
+            .max(1000),
+        category: Joi.string()
             .required()
             .valid(
                 "Thriller",
@@ -90,22 +94,22 @@ function validateBook(book) {
                 "Travel",
                 "Non-fiction"
             ),
-        publishingYear: Joi.Number
+        publishingYear: Joi.number()
             .required()
             .integer()
-            .min(0)
-            .max(5),
-        reviewsList: Joi.array().items(
-            Joi.object({
-                user: Joi.string(),
-                body: Joi.string().max(200),
-                date: Joi.date(),
-                stars: Joi.Number.validate(1, 2, 3, 4, 5)
-            })
-        )    
+            .max(currentYear), 
     }
     return Joi.validate(book, schema);
 };
 
+function validateComment(book) {
+    const schema = {
+        comment: Joi.string().max(200).min(5).required(),
+        stars: Joi.number().min(0).max(5).integer().required()
+    }
+    return Joi.validate(book, schema);
+}
+
 exports.Book = Book;
-exports.validate = validateBook;
+exports.validateBook = validateBook;
+exports.validateComment = validateComment;
