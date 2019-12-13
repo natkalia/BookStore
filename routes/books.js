e1edd23c4e8eded1d2c6e16391168894b1d8b204
Agata Ceglarz <pazagata@gmail.com>
2019-12-13 20:40:05 +0100

    Update: Add 10ten books  - without view fix

--- routes/books.js
+++ routes/books.js
@@ -41,4 +41,32 @@ router.get('/edit/:id', async (req, res) => {
   })
 })
 
+//Top10 books
+router.get('/', async (req, res) => {
+  const books = await Book.find();
+  if (!books) return res.status(404).send('There are no books found');
+
+  let top10books = [];
+  let sortedBooks= [];
+  for (book of books) {
+    if (book.reviewsList.length !== 0){
+      let starsTotalAmount = 0;
+      for (review of book.reviewsList) {
+        starsTotalAmount += review.stars;
+      }
+      const avarageNote = (starsTotalAmount / book.reviewsList.length).toFixed(2);
+      sortedBooks.push({book, avarageNote})
+    }
+  }
+
+  sortedBooks.sort((a, b) => b.avarageNote - a.avarageNote);
+  top10books = sortedBooks.slice(0, 11);
+
+  res.render('top10books', {
+    top10books: top10books
+  })
+});
+
+
+
 module.exports = router;
