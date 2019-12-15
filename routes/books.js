5c00465d7a8a63a126e92fdce2b02ef4a89ce420
Agata Ceglarz <pazagata@gmail.com>
2019-12-15 18:56:35 +0100

    Update: Change path

--- routes/books.js
+++ routes/books.js
@@ -114,32 +114,5 @@ router.post('/:id', async (req, res) => {
   res.redirect('/api/books/'+ book._id);
 });
 
-//Top10 books
-router.get('/', async (req, res) => {
-  const books = await Book.find();
-  if (!books) return res.status(404).send('There are no books found');
-
-  let top10books = [];
-  let sortedBooks= [];
-  for (book of books) {
-    if (book.reviewsList.length !== 0){
-      let starsTotalAmount = 0;
-      for (review of book.reviewsList) {
-        starsTotalAmount += review.stars;
-      }
-      const avarageNote = (starsTotalAmount / book.reviewsList.length).toFixed(2);
-      sortedBooks.push({book, avarageNote})
-    }
-  }
-
-  sortedBooks.sort((a, b) => b.avarageNote - a.avarageNote);
-  top10books = sortedBooks.slice(0, 11);
-
-  res.render('main', {
-    top10books: top10books
-  })
-});
-
-
 
 module.exports = router;
