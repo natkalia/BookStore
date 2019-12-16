870db784a63bd2c93d7cbe9e3f0090f073240ddb
anna <annapiwonska1@gmail.com>
2019-12-16 12:08:02 +0100

    Add basic route handling for get requests for login and register forms

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const top10 = require('./routes/top10');
 const books = require('./routes/books');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 const app = express();
 
 const connectionString = `mongodb+srv://${config.get('db.user')}:${config.get('db.password')}@${config.get('db.address')}/${config.get('db.name')}?retryWrites=true&w=majority`;
 
 mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => dbDebug('Connected to MongoDB...'))
   .catch((err) => {
     dbDebug('Could not connect to MongoDB.', err.message);
@@ -24,13 +26,17 @@ mongoose.connect(connectionString, {
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
   basicDebug('Morgan enabled...')
 }
 app.use(express.static('public'));
 app.use('/api/books', books);
+app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 /*
 app.use('/api/users', users);
